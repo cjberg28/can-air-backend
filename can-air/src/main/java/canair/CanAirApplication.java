979637361ed7a5fbd967083713f0446584a61ca2
cjberg28 <cjberg28@yahoo.com>
2022-07-26 12:22:34 -0500

    Added template Spring Boot project.

---
+++ can-air/src/main/java/canair/CanAirApplication.java
@@ -0,0 +1,13 @@
+package canair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanAirApplication.class, args);
+	}
+
+}
---
+++ can-air/src/test/java/canair/CanAirApplicationTests.java
@@ -0,0 +1,13 @@
+package canair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
