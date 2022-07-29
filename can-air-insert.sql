5adc20c23ee7e1346d58517c090b5c6ce9a1fe12
Atul Mishra <69535893+friedfishra@users.noreply.github.com>
2022-07-29 18:23:26 -0400

    added dummy data for return trip to insert.sql

--- can-air-insert.sql
+++ can-air-insert.sql
@@ -9,17 +9,24 @@ VALUES
 ('MSP'),
 ('LAX');
 
-INSERT INTO flight (flightId, startid, endid, departuredate, departuredeparturetime, departurearrivaltime, returndate, returndeparturetime, returnarrivaltime, isroundtrip, flightprice, flightcapacity) 
-VALUES 
-(1, 1, 2, '2022-11-30', '07:30', '11:30', null, null, null, false, 7000.00, 50); 
 
 INSERT INTO flight (startid, endid, departuredate, departuredeparturetime, departurearrivaltime, returndate, returndeparturetime, returnarrivaltime, isroundtrip, flightprice, flightcapacity) 
-VALUES 
+VALUES
+( 1, 2, '2022-11-30', '07:30', '11:30', null, null, null, false, 7000.00, 50),  
 ( 1, 2, '2022-12-01', '09:30', '13:30', null, null, null, false, 7000.00, 50), 
 ( 2, 1, '2022-12-02', '10:30', '14:30', null, null, null, false, 7000.00, 50), 
 ( 2, 1, '2022-12-03', '06:00', '10:30', null, null, null, false, 8000.00, 50), 
 ( 1, 2, '2022-12-04', '15:00', '19:30', null, null, null, false, 6000.00, 50), 
-( 1, 2, '2022-12-05', '00:00', '04:30', null, null, null, false, 1000.00, 1) 
+( 1, 2, '2022-12-05', '00:00', '04:30', null, null, null, false, 1000.00, 1),
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-02', '8:00', '12:00', true, 7000.00, 50), 
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-03', '8:00', '12:00', true, 7000.00, 50), 
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-04', '8:00', '12:00', true, 7000.00, 50), 
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-05', '8:00', '12:00', true, 7000.00, 50), 
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-06', '8:00', '12:00', true, 7000.00, 50),
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-07', '8:00', '12:00', true, 7000.00, 50),
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-02', '14:00', '18:00', true, 6000.00, 50),
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-04', '14:00', '18:00', true, 6000.00, 50),
+( 1, 2, '2022-12-01', '09:30', '13:30', '2022-12-06', '14:00', '18:00', true, 6000.00, 50)
 
 ;
 
